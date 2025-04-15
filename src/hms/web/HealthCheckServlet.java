097fd8d84c4b577f655224694d911e6501cc6816
Min-Hua Chao <legion625@gmail.com>
2025-04-16 00:30:25 +0800

    test

--- src/hms/web/HealthCheckServlet.java
+++ src/hms/web/HealthCheckServlet.java
@@ -7,13 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HealthCheckServlet extends HttpServlet {
+	private Logger log  = LoggerFactory.getLogger(HealthCheckServlet.class);
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		log.debug("HealthCheckServlet.doGet 1");
 		// 設定回應內容為純文字格式
 		response.setContentType("text/plain");
 		// 寫入回應訊息 "OK"
 		response.getWriter().write("OK");
+		log.debug("HealthCheckServlet.doGet 2");
 	}
 }
